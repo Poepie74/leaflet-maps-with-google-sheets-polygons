451f0737b34d9853bdefc40314878a1cb0c10ccc
Poepie74 <40661792+Poepie74@users.noreply.github.com>
2018-06-29 15:07:20 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1FL13bzvFXiMWJWh2NL1WmOak3nrQ-M-McbwM5qWrASs/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xSLs6JxVn8-jUaj78cEw0tGL6hlClvhJ-iZiuXU3qdo/edit#gid=0';
